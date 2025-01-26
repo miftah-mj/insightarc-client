03c3f2a8ae0dac6aab55a5201f7f66469b33301a
miftah-mj <jannat27120@gmail.com>
2025-01-26 16:54:14 +0600

    add search functionality and improve article display

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,36 +1,61 @@
-import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
+import Button from "./common/Button";
 
 const Card = ({ article }) => {
-    const { _id, title, image, publisher, tags } = article || {};
+    const {
+        _id,
+        title,
+        image,
+        publisher,
+        description,
+        isPremium,
+        userHasSubscription,
+    } = article;
+    const navigate = useNavigate();
 
     return (
-        <Link
-            to={`/article/${_id}`}
-            className="col-span-1 cursor-pointer group shadow-xl p-3 rounded-xl"
+        <div
+            className={`col-span-1 cursor-pointer group shadow-xl p-3 rounded-xl ${
+                isPremium ? "bg-yellow-100" : "bg-white"
+            }`}
         >
             <div className="flex flex-col gap-2 w-full">
                 <div className="aspect-square w-full relative overflow-hidden rounded-xl">
                     <img
                         className="object-cover h-full w-full group-hover:scale-110 transition"
                         src={image}
-                        alt="Plant Image"
+                        alt={title}
                     />
-                    <div className="absolutetop-3right-3"></div>
+                    <div className="absolute top-3 right-3"></div>
                 </div>
                 <div className="font-semibold text-lg">{title}</div>
                 <div className="font-semibold text-lg">
                     Publisher: {publisher}
                 </div>
-                <div className="font-semibold text-lg">
-                    {tags.map((tag, index) => (
-                        <span key={index} className="mr-2">
-                            #{tag}
-                        </span>
-                    ))}
+                <div className="text-gray-700">
+                    {`${description.slice(0, 50)}...`}
                 </div>
+                {/* <button
+                        className={`mt-2 p-2 rounded ${
+                            isPremium && !userHasSubscription
+                                ? "bg-gray-300 cursor-not-allowed"
+                                : "bg-blue-500 text-white"
+                        }`}
+                        disabled={isPremium && !userHasSubscription}
+                    >
+                        {isPremium && !userHasSubscription
+                            ? "Subscribe to view"
+                            : "View Details"}
+                    </button> */}
+
+                <Button
+                    label={"Details"}
+                    disabled={isPremium && !userHasSubscription}
+                    onClick={() => navigate(`/article/${_id}`)}
+                />
             </div>
-        </Link>
+        </div>
     );
 };
 
@@ -40,7 +65,9 @@ Card.propTypes = {
         title: PropTypes.string.isRequired,
         image: PropTypes.string.isRequired,
         publisher: PropTypes.string.isRequired,
-        tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+        description: PropTypes.string.isRequired,
+        // isPremium: PropTypes.bool.isRequired,
+        // userHasSubscription: PropTypes.bool.isRequired,
     }).isRequired,
 };
 
--- src/pages/Articles/Articles.jsx
+++ src/pages/Articles/Articles.jsx
@@ -1,25 +1,38 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { useState } from "react";
 import Card from "../../components/Card";
 import Container from "../../components/common/Container";
 import LoadingSpinner from "../../components/common/LoadingSpinner";
 
 const Articles = () => {
+    const [searchTerm, setSearchTerm] = useState("");
+
     const { data: articles, isLoading } = useQuery({
-        queryKey: ["articles"],
+        queryKey: ["articles", searchTerm],
         queryFn: async () => {
             const response = await axios(
-                `${import.meta.env.VITE_API_URL}/articles`
+                `${import.meta.env.VITE_API_URL}/articles?search=${searchTerm}`
             );
             return response.data;
         },
     });
-    console.log(articles);
-
     if (isLoading) return <LoadingSpinner />;
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        setSearchTerm(e.target.value);
+    };
 
     return (
         <Container>
+            <input
+                type="text"
+                placeholder="Search articles..."
+                value={searchTerm}
+                onChange={handleSearch}
+                className="w-1/2 mb-4 p-2 px-4 border border-indigo-600 rounded"
+            />
             {articles && articles.length > 0 ? (
                 <div className="pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
                     {articles.map((article) => (
